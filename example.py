a89963339f69f336a6d4c6fe565ca51e49e5e403
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-06-25 09:47:18 +0200

    created app, installed CSnakes

---
+++ example.py
@@ -0,0 +1,23 @@
+number = 1
+number2 = 2
+
+print(number + number2)
+
+print(1+2)
+
+number = 'hello'
+
+
+def sum_two_numbers(num, num2): 
+    return num + num2
+
+result = sum_two_numbers(1,2)
+print(result)
+
+class MyClass:
+    age: str
+    id: int
+    def __init__(self, age: str, id: int):
+        self.age = age
+        self.id = id
+        pass
